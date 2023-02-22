321afa46ef0e6a5ee12b0c174118aeb02f1e7df1
Prince-Motsumi <princemoscie@gmail.com>
2023-02-22 14:33:39 +0200

    added more properties for contact

--- src/main/java/com/digableprince/digableprofile/contact/Contact.java
+++ src/main/java/com/digableprince/digableprofile/contact/Contact.java
@@ -1,15 +1,29 @@
 package com.digableprince.digableprofile.contact;
 
+import java.time.LocalDate;
+
 public class Contact {
+	private Integer id;
 	private String name;
 	private String email;
 	private String messsage;
+	private LocalDate createdAt;
 
-	public Contact(String name, String email, String messsage) {
+	public Contact(Integer id, String name, String email, String messsage, LocalDate createdAt) {
 		super();
+		this.id = id;
 		this.name = name;
 		this.email = email;
 		this.messsage = messsage;
+		this.createdAt = createdAt;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
 	}
 
 	public String getName() {
@@ -36,9 +50,18 @@ public class Contact {
 		this.messsage = messsage;
 	}
 
+	public LocalDate getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDate createdAt) {
+		this.createdAt = createdAt;
+	}
+
 	@Override
 	public String toString() {
-		return "contact [name=" + name + ", email=" + email + ", messsage=" + messsage + "]";
+		return "Contact [id=" + id + ", name=" + name + ", email=" + email + ", messsage=" + messsage + ", createdAt="
+				+ createdAt + "]";
 	}
 
 }

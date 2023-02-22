c1385d294173dc0de4728236117a6f72319290dc
Prince-Motsumi <princemoscie@gmail.com>
2023-02-22 13:46:21 +0200

    init commit

---
+++ src/main/java/com/digableprince/digableprofile/DigableProfileApplication.java
@@ -0,0 +1,13 @@
+package com.digableprince.digableprofile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigableProfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigableProfileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digableprince/digableprofile/DigableProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digableprince.digableprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigableProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
